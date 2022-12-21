51544355ab19657df790e6392ec3df066e998bce
martin mithi <gathitumithi9000@gmail.com>
2022-12-21 11:42:50 +0300

    commit tree 4c5bcebe7aaa7d506f93d9076f7dfae41ef4c8bf

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
-export function manageFriends(state, action) {
+export function manageFriends(state = { friends: [] }, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      const remainingFriends = state.friends.filter(
+        (myFriend) => myFriend.id !== action.payload
+      );
+      return { friends: remainingFriends };
+    default:
+      return state;
+  }
 }
